4b430a8d6d37fd921bd3f706074968cb3566fffd
Almasyriqi <ikramalmasyriqi@gmail.com>
2023-05-02 14:40:08 +0700

    add requirements file and endpoint user detail

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from flask import flash, request
 @app.route('/user')
 def user():
     try:
-        _json = request.args
-        print(_json['data'])
+        _param = request.args
+        _form = request.form
+        print("Request data dari param : " + _param['data'])
+        print("Request data dari form : " + _form['data'])
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute("SELECT * FROM user")
         userRows = cursor.fetchall()
         respon = jsonify(userRows)
         respon.status_code = 200
-        print("coba print aja")
         return respon
     except Exception as e:
         print(e)
@@ -23,6 +24,22 @@ def user():
         cursor.close()
         conn.close()
         
+@app.route('/user/<int:user_id>')
+def user_details(user_id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM user WHERE id =%s", user_id)
+        empRow = cursor.fetchone()
+        respone = jsonify(empRow)
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close() 
+
 @app.errorhandler(404)
 def showMessage(error=None):
     message = {
